b0b35a07a9e3a0a4d565793cd5d035eb760693b1
github-cloudlabsuser-2065 <github_cloudlabsuser_2065@cloudlabsaiuser.com>
2024-05-29 08:55:18 +0000

    Now working

--- weather_script.py
+++ weather_script.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_weather(city):
-    api_key = "7f0d568faab065b97d0b960e1693f163"  # Replace with your OpenWeatherMap API key
+    api_key = "4b8b598d937caf6b13933803cee77dbd"  # Replace with your OpenWeatherMap API key
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city,
@@ -16,7 +16,6 @@ def get_weather(city):
         # Process the weather data as per your requirements
         # For example, you can extract temperature, humidity, etc.
         temperature = weather_data["main"]["temp"]
-        #humidity = weather_data["main"]["humidity"]
         # Return the weather information
         return temperature
     except requests.exceptions.RequestException as e:
@@ -25,6 +24,6 @@ def get_weather(city):
 
 # Example usage
 city = "London"  # Replace with the desired city
-temperature = get_weather("Stockholm")
-# if temperature and humidity:
-#    print(f"The temperature in {city} is {temperature}°C with {humidity}% humidity.")
+temperature = get_weather(city)
+if temperature:
+    print(f"The temperature in {city} is {temperature}°C.")
